e5e643a0cc2be4941717778cd9d2fb328ae237d3
yudujie <maxroot.neo.io@outlook.com>
2026-04-05 20:04:05 +0800

    add Exception when error

---
+++ tlias-web-management/src/main/java/maxroot/neo/io/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package maxroot.neo.io.exception;
+
+import maxroot.neo.io.pojo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //处理异常
+    @ExceptionHandler
+    public Result ex(Exception e){//方法形参中指定能够处理的异常类型
+        //打印堆栈中的异常信息
+        e.printStackTrace();
+
+        //捕获到异常之后，响应一个标准的Result
+        return Result.error("对不起,操作失败,请联系管理员");
+    }
+
+}
